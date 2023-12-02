0dc5e0a8007aa64520b4b4ef0144dec7ad80c4fe
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-02 17:12:36 +0530

    Login response and state set null when page changing

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,8 +18,8 @@ function Register() {
     }else{
       setErr('')
       axiosInstance.post('/login',{email,password}).then(res=>{
-        console.log(res.data);   
-        navigate('/')
+        console.log(res.data);  
+        navigate('/home')
       }).catch(err=>{
         if(err?.response?.data?.errMsg){
           toast.error(err?.response?.data?.errMsg)
@@ -63,16 +63,18 @@ function Register() {
           <form className="space-y-4 md:space-y-6">
             <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-              <input type="email" onChange={(e)=>setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+              <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
             </div>
             <div>
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input type="password" onChange={(e)=>setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+              <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
             <div className="flex justify-center h-3">
             <span className="text-xs text-center text-red-600">{err}</span>
             </div>
             <button onClick={()=>{
+              setEmail('')
+              setPassword('')
               loginPage?handleLogin():handleSignUp()
             }} type="button" className="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{loginPage?'Sign in':'Sign up'}</button>
             <p onClick={()=>{loginPage?setLoginPage(false):setLoginPage(true)}} className="text-sm text-center hover:cursor-pointer font-light text-gray-500 dark:text-gray-400">
