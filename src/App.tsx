8ab8732e43deec7723b42b8d14999f1474a6b0ac
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-05 16:55:36 +0530

    token checking change to 'undefined'

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import Notes from './pages/Notes'
 function App() {
 
   const token = localStorage.getItem('token')
+
   return (
     <Router>
       <Routes>
-        <Route path="/signIn" element={token ? <Navigate to={'/'} /> : <Register />} />
-        <Route path="/" element={token ? <Notes /> : <Navigate to={'/signIn'} />} />
+        <Route path="/signIn" element={token !== 'undefined' ? <Navigate to={'/'} /> : <Register />} />
+        <Route path="/" element={token !== 'undefined' ? <Notes /> : <Navigate to={'/signIn'} />} />
       </Routes>
     </Router>
   )
